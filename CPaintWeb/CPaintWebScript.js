3ca7ed718aab6eb3f0ddb98cec239836c24add26
Devon Loehr <devon.loehr@gmail.com>
2015-04-04 16:21:51 -0400

    Can draw points

--- CPaintWeb/CPaintWebScript.js
+++ CPaintWeb/CPaintWebScript.js
@@ -1,4 +1,4 @@
-function Init() {
+unction Init() {
 	// Add the parameter boxes to the form	
 	for(iii = "A".charCodeAt(0); iii < "Z".charCodeAt(0); iii++) {
 		var newBox = document.createElement("div");
@@ -8,6 +8,9 @@ function Init() {
 				   String.fromCharCode(iii) + "'><br>";
 		document.getElementById("vars").appendChild(newBox);
 	}
+	
+	// Draw the basic lines on the grids
+	DrawGrids();
 }
 
 function UpdateEquation() {
@@ -17,7 +20,7 @@ function UpdateEquation() {
 	// Hide or display relevant variable boxes
 	for(iii = "A".charCodeAt(0); iii < "Z".charCodeAt(0); iii++) {
 		if(eqStr.indexOf(String.fromCharCode(iii)) != -1) // If this character is in the string
-			document.getElementById(String.fromCharCode(iii)).style.display = "inline"; // Show it's box
+			document.getElementById(String.fromCharCode(iii)).style.display = "inline"; // Show its box
 		else 
 			document.getElementById(String.fromCharCode(iii)).style.display = "none"; // Don't
 	}
@@ -53,9 +56,61 @@ function CopyGraph(direction) {
 }
 
 function ClearGraph() {
+	DrawGrids();
+}
+
+function DrawGrids() {
+	var grid = document.getElementById("leftGraph");
+	var ctx = grid.getContext("2d");
+	ctx.moveTo(0, grid.height / 2);
+	ctx.lineTo(grid.width, grid.height / 2);
+	ctx.stroke();
+	ctx.moveTo(grid.width / 2, 0);
+	ctx.lineTo(grid.width / 2, grid.height);
+	ctx.stroke();
+	
+	grid = document.getElementById("rightGraph");
+	ctx = grid.getContext("2d");
+	ctx.moveTo(0, grid.height / 2);
+	ctx.lineTo(grid.width, grid.height / 2);
+	ctx.stroke();
+	ctx.moveTo(grid.width / 2, 0);
+	ctx.lineTo(grid.width / 2, grid.height);
+	ctx.stroke();
+}
+
+function DrawPoint(point, left) {
+	var grid;
+	if(left)
+		grid = document.getElementById("leftGraph");
+	else
+		grid = document.getElementById("rightGraph");
+	var ctx = grid.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(point.x, point.y, 10, 0, 2*Math.PI);
+	ctx.fillStyle = "#333";
+	ctx.fill();
+}
+
+// Currently only works if centered at (0,0)
+function MathToGraph(point, left) {
+	var grid;	
+	if(left)		
+		grid = document.getElementById("leftGraph");
+	else
+		grid = document.getElementById("rightGraph");
+	var newpt = {x:0, y:0};
+	newpt.x = point.x + grid.width / 2;
+	newpt.y = grid.height / 2 - point.y;
+	return newpt;
+}
+
+function EvalString() {
 	return;
 }
 
 function foo() {
-	document.getElementById("foopar").innerHTML += (document.getElementById("currentMode").innerHTML == "Point");
+	//document.getElementById("foopar").innerHTML += 0;
+	var pt = {x:25, y:25};
+	DrawPoint(WindowToGraph(pt, 1), 1);
 }
